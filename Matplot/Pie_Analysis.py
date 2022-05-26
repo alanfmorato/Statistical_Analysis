d5dc5a70c2d44ed48cb6c380b76f7eb419b2794e
Alan Morato <morato291096@gmail.com>
2022-05-26 15:49:41 -0300

    Pie Analysis

---
+++ Matplot/Pie_Analysis.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+valores = [15, 30, 45, 10]
+
+rotulos = ['Apple', 'Banana', 'Orange', 'Pineapple']#Definindo legendas
+
+explode = [.1, .1, .1, .1]#Definindo explosão do gráfico
+
+colors = ['black', 'orange', 'green', 'red']
+
+plt.figure(figsize=[5,5])#Definindo tamanho da imagem
+
+
+plt.pie(valores, explode=explode, labels=rotulos, colors=['blue', 'orange', 'green', 'red'], autopct='%.1f%%', shadow=True, startangle=90)#Definindo o gráfico de pizza
+
+plt.show()#Exibindo o gráfico
+
